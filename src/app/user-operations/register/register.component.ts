061f7427f0a1078274dbd3f7fd8bdac0ab289385
admin <admin123@gmail.com>
2018-11-28 01:27:20 +0530

    Added the country select functionality in registration page

--- src/app/user-operations/register/register.component.ts
+++ src/app/user-operations/register/register.component.ts
@@ -7,25 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   country = "india";
-
+  countries: Array<any>[];
+  selectedStates: Array<any> = [];
+  citiesList: Array<any> = [];
+  selectedCountries: Array<any> = [];
   countriesList: Array<any> = [
-    { name: 'Germany', states: ['Duesseldorf', 'Leinfelden-Echterdingen', 'Eschborn'], countries: ['germany country'] },
-    { name: 'Spain', states: ['Barcelona'], countries:['spain country'] },
-    { name: 'india', states: ['westGodavari'], countries:['marteru'] },
-    { name: 'USA', states: ['Downers Grove'],countries:['usa countries'] },
-    { name: 'Mexico', states: ['Puebla'], countries:['mexico countries'] },
-    { name: 'China', states: ['Beijing'], countries:['china countries'] },
-  ];
-  selectedStates: Array<any>;
-  changeState(selectedstate){
-    this.selectedStates = this.countriesList.find(x => x.name == selectedstate).states;
+    {
+      name: 'Germany', states: [{ stateName: 'state1', countryNames: ["country1", "country2"] }, { stateName: 'state2', countryNames: ["country3", "country4"] }]
+    },
+    {
+      name: 'india', states: [{ stateName: 'west', countryNames: ["marteru", "palkol"] }, { stateName: 'east', countryNames: ["rvpm", "rjy"] }]
+    }
+    ];
+  statesList: Array<any> = [];
+  changeState(selectedstate) {
+    this.selectedStates = [];
+    for (let i = 0; i < this.countriesList.find(x => x.name == selectedstate).states.length; i++) {
+      this.statesList = this.countriesList.find(x => x.name == selectedstate).states[i].stateName;
+      this.selectedStates.push(this.statesList);
+    }
   }
 
-  selectedCountries:Array<any>;
-  changeCountry(selectedcountry){
-    console.log(this.countriesList.find(x => x.states));
-    
-    this.selectedCountries = this.countriesList.find(x => x.states[selectedcountry  ] == selectedcountry ).countries;
+
+
+  changeCountry(selectedcountry) {
+    for (let i = 0; i < this.selectedStates.length; i++) {
+      if (this.countriesList.find(x => x.states[i].stateName == selectedcountry)) {
+        let citiesCount = this.countriesList.find(x => x.states[i].stateName == selectedcountry).states[i].countryNames.length;
+        for (let t = 0; t < citiesCount; t++) {
+          this.selectedCountries[t] = this.countriesList.find(x => x.states[i].stateName == selectedcountry).states[i].countryNames[t];
+        }
+      }
+
+    }
+    console.log(this.selectedCountries);
+
+    // this.selectedCountries = this.countriesList.find(x => x.states[selectedcountry] == selectedcountry).countries;
   }
 
   constructor() { }
